2e6d8681fb3b80d3cf121f5ec6de018aa15362d6
ng1986 <niko@geschka.org>
2021-09-30 00:48:49 +0200

    Update ping_server.sh

--- ping_server.sh
+++ ping_server.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# $ ./ping_server.sh                                                 
+# waiting for ServerXY ....................
+# Server is back online
+
+
+printf "%s" "waiting for ServerXY ..."
+while ! ping -c 1 -n -w 1 147.153.237.192 &> /dev/null
+do
+    printf "%c" "."
+done
+printf "\n%s\n"  "Server is back online"
