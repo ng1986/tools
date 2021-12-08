036c10f65ae10275c883684a4a8687d121a934d4
ng1986 <niko@geschka.org>
2021-12-08 16:01:52 +0100

    Create hardware.py

---
+++ hardware.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sqlite3
+
+con = sqlite3.connect('hardware.db')
+
+cur = con.cursor()
+
+#for row in cur.execute('SELECT * FROM INTERFACES'):
+#    print(row)
+
+con.close()
